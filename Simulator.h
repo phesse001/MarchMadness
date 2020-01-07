0631f34c449c15df56654c6f1c2f783414d9939c
wbonsu <34726639+wbonsu@users.noreply.github.com>
2020-01-07 12:25:39 -0500

    Write output to txt update

--- Simulator.h
+++ Simulator.h
@@ -46,5 +46,5 @@ void checkCorrectness(string correctData);
 
 void bin(vector<Team*> *teamVector, vector<vector<int>> *resultVector);
 
-double comparison(vector<vector<int>> *v1, vector<vector<int>> *v2);
+void comparison(vector<vector<int>> *v1, vector<vector<int>> *v2);
 #endif //MARCH_MADNESS_SIMULATOR_SIMULATOR_H
